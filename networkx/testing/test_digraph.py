873ae0a04af91341e409dc660b7f923082d6e20c
novium <mail@novium.pw>
2019-12-04 10:47:26 +0100

    Add node-addition tests, todo: importing numpy/scipy/networkx data as graphs

--- networkx/testing/test_digraph.py
+++ networkx/testing/test_digraph.py
@@ -10,7 +10,30 @@ class TestDigraph(unittest.TestCase):
         self.assertIsNotNone(G)
 
         G = nx.DiGraph(name = 'test_create')
+        self.assertIsNotNone(G)
         self.assertEqual(G.name, 'test_create')
 
-    def test_adj_1(self):
-        pass
+    def test_add_node_1(self):
+        G = nx.DiGraph()
+        self.assertIsNotNone(G)
+
+        # New graph
+        self.assertEqual(G.number_of_nodes(), 0)
+        G.add_node(0) # Add a node, check if added
+        self.assertEqual(G.number_of_nodes(), 1)
+        G.add_node(0) # Check that same node can't be added twice (branch)
+        self.assertEqual(G.number_of_nodes(), 1)
+        G.add_node(1) # Add >1 node
+        self.assertEqual(G.number_of_nodes(), 2)
+
+    def test_add_nodes_from_1(self):
+        G = nx.DiGraph()
+        self.assertIsNotNone(G)
+
+        nodes = [0,2,3,5,7,11,13,17]
+        self.assertEqual(G.number_of_nodes(), 0)
+        G.add_nodes_from(nodes)
+        self.assertEqual(G.number_of_nodes(), len(nodes))
+        nodes_p = nodes[0:2]
+        G.add_nodes_from(nodes_p)
+        self.assertEqual(G.number_of_nodes(), len(nodes))
